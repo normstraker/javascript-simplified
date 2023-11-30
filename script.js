b828d56b761eb66867b1f955285bd82bcb35ba43
Norm Straker <normstraker@yahoo.com>
2023-11-30 12:04:07 -0600

    17 - Passing Functions as Arguments - end

--- script.js
+++ script.js
@@ -12,14 +12,24 @@
 // 1. name
 // 2. callback that prints out what we pass to it (printVariable)
 
-function w(x, y) {
-  y(x);
+function w(x, callback) {
+  callback("Hello " + x);
 }
-function aa(z) {
-  console.log("Hello " + z);
-}
-function bb(v) {
-  console.log("What's up " + v + "?");
-}
-w("Norm", aa);
-w("Norman", bb);
+
+// function aa(z) {
+//   console.log("Hello " + z);
+// }
+// function bb(v) {
+//   console.log("What's up " + v + "?");
+// }
+// w("Norm", aa);
+// w("Norman", bb);
+
+// or
+
+w("Norm", function (z) {
+  console.log(z);
+});
+
+// passing a function to a function is just making the function a fancy variable
+// when you pass a function as a variable don't use the ()
