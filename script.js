060a880ec1cbc088bd0f1b867187b6636b8d1ed7
Norm Straker <normstraker@yahoo.com>
2023-11-30 11:02:19 -0600

    17 - Passing Functions as Arguments

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-let a = null
-let b = undefined
+// function sumCallback(a, b, callback) {
+//   callback(a + b);
+// }
 
-console.log(a != b);
+// function handleSum(sum) {
+//   console.log(sum);
+// }
+
+// sumCallback(2, 3, handleSum);
+
+// create a new function that takes two parameters;
+// 1. name
+// 2. callback that prints out what we pass to it (printVariable)
+
+function w(x, y) {
+  y(x);
+}
+function aa(z) {
+  console.log("Hello " + z);
+}
+function bb(v) {
+  console.log("What's up " + v + "?");
+}
+w("Norm", aa);
+w("Norman", bb);
