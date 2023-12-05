2d6e132b1222e3a6f8a5a48a0b9fee6d4068b6ea
Norm Straker <normstraker@yahoo.com>
2023-12-05 12:28:58 -0600

    19 - Stack Trace and Call Stack

--- script.js
+++ script.js
@@ -1,57 +1,18 @@
-// regular function
-// function sum(a, b, c) {
-//   return a + b - c;
-// }
-// console.log(sum(5, 6, 1));
-
-// let dum = (a, b, c) => {
-//   return a - b + c;
-// };
-// console.log(dum(1, 10, 19));
-
-// let aName = x => {
-//   return x;
-// };
-// console.log(aName("Norman Lewis Straker"));
-
-// if setup in settings, () are removed around single parameter with arrow function (ie x)
-let aName = x => {
-  console.log(x);
-};
-aName("Norm");
-
-// with arrow function if code on one line you can leave off the {} and 'return' and js will return a + b value
-let aSum = (a, b) => a + b;
-console.log(aSum(6, 9));
-
-// same as
-
-// let aSum = (a, b) => {
-//   return a + b;
-// };
-// console.log(aSum(6, 9));
-
-let printHi = name => "Hi " + name;
-console.log(printHi("Norm"));
-
-// all variables and function names must be unique
-// function Hi() {
-//   console.log("hi");
-// }
-// Hi();
-
-// how to write an arrow function with empty parameter
-let hiYa = () => {
-  console.log("hi");
-};
-hiYa();
-
-// why arrow functions?
-function func(x, callback) {
-  callback(x);
+function doStuff(a, b, name) {
+  print(sum(a, b));
+  print(sayHi(name));
 }
-// func(10, function (variable) {
-//   console.log(variable);
-// });
-// to this
-func(10, variable => console.log(variable))
+
+function print(variable) {
+  console.log(variable);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function sayHi(name) {
+  return "Hi " + name;
+}
+
+doStuff(1, 2, "Norm");
