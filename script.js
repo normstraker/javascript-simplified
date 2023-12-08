d9b7b6578fe2db3d918e9f452f549720fddbd6e1
normstraker <88446816+normstraker@users.noreply.github.com>
2023-12-08 16:05:42 -0600

    23 - Const

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// let a = 1;
+// Variables
+// let variable lets you know it can change
+let a = 1
 
-// function print() {
-//   console.log(a);
-// }
-
-// a = 2;
-// print();
-
-function print(variable) {
-  let c = 3;
-  return function func(variable2) {
-    console.log(variable); //1
-    console.log(variable2); //2
-    console.log(c); //3
-  };
-}
-let a = print(1);
-a(2); //print(1) func(2)
-
-// https://chat.openai.com/g/g-igUACg05d-web-design-guru/c/1a10e6cb-1076-45e5-becd-d2e1e6b7b59e
+// const variable value cannot be changed (lets you know this was meant not to change)
+const b = 2
