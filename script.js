837d1c4cc574b52c8e15cbe0639fc433b8b92251
Norm Straker <normstraker@yahoo.com>
2023-12-11 17:02:10 -0600

    27 - Nan

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// == VS ===
+//Nan
+const a = parseInt("sdfsdf");
+const b = 1;
 
-const a = 1;
-const b = "1";
-const c = 0;
-const d = false;
-const f = null;
-const g = undefined;
-// using == (JavaScript still converts types)
-console.log(a == b); // true
-console.log(c == d); // true
-// using === (JavaScript does not convert 2 different types)
-console.log(a === b); // false
-console.log(c === d); // false
-// use parseInt with === to convert, don't let JS do it using just ==
-console.log(a === parseInt(b)); // true
-
-console.log(f == g); // true (they are the same in JS eyes - they both mean 'nothing')
-console.log(f === g); // false (since they are technically different)
-
-// these rules apply to using != and !== (not equal) as well
+console.log(a);
+// anything equal to Nan is always Nan
+console.log(a === NaN);
+// you need to use a special method to determine if something is not-not a number (isNaN)
+console.log(isNaN(b));
