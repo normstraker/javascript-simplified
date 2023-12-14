cc531c794876f32dbd21219f0e1dc4410d2f02dc
Norm Straker <normstraker@yahoo.com>
2023-12-14 11:21:51 -0600

    30 - Reference VS Value

--- script.js
+++ script.js
@@ -1,53 +1,59 @@
-// Objects - a collection of information (properties) related in some way
+// Reference VS Value
+// Variables are assigned values
+// Arrays and Objects are assigned references in a computer memory location
 
-let name = "Norm";
-const age = 57;
-const favoriteNum = 127;
+// let a = 10;
+// let b = "Hi";
+// let c = [1,2]; // eg. 0x01
+// let d = c; // eg. 0x01
+// d.push(3)
 
-// To create an object you use {}
-let person = {
-  name: "Norm",
-  age: 57,
-  favoriteNum: 127,
-  // you can add a function
-  sayHi() {
-    console.log("Hi");
-  },
-  property: "Value",
-};
-// let person = { name, age, favoriteNum, sayHi };
-console.log(person);
-console.log(person.sayHi);
-person.sayHi();
+// let a = 10;
+// let b = "Hi";
+// let c = [1, 2]; // eg. 0x01
+// let d = [3, 4, 5]; // eg. 0x02 (new memory)
+// d.push(6);
 
-// Exercise
-// Create an object with the properties:
-// 1. Make
-// 2. Model
-// 3. isUsed
-// function that logs "Vrooom!"
+let a = 10;
+let b = "Hi";
+let c = { name: "Norm" }; // eg. 0x01
+let d = c; // eg. 0x01
+d.name = "Joe";
 
-let car = {
-  Make: "Ford",
-  Model: "F150",
-  isUsed: true,
-  makeNoise() {
-    console.log("Vrooom!");
-  },
-};
-console.log(car.Make, car.Model, car.isUsed);
-car.makeNoise();
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + JSON.stringify(c)); // prints out an object
+console.log("d = " + JSON.stringify(d));
 
-// Exercise
-const book = {
-  title: "How the West was Won",
-  Author: { name: "Pete Shmeet", age: 63 },
-  wives: ["Loretta", "Jamie"],
-};
-console.log(book.title, book.Author.name, book.Author.age);
+// ========
 
-// you can redefine object properties and array values
-book.title = "Napolean";
-book.Author.age = 73;
-book.wives[1] = "Myrtle";
-console.log(book.title, book.Author.name, book.Author.age, book.wives[1]);
+let aa = [1, 2]; // 0x01
+let bb = [1, 2]; // 0x02
+console.log(aa === bb); // false
+
+aa.push(3);
+console.log(aa);
+console.log(bb);
+
+// =========
+const aaa = [1, 2]; // 0x01
+const bbb = [1, 2]; // 0x02
+aaa.push(3); // pushes to the memory, doesn't change location (const)
+// aaa = [1, 2, 3] // throws an error, redefined, not modified
+console.log(aaa);
+console.log(bbb);
+
+// ==========
+
+const z = [1, 2]; // 0x01
+const elementToAdd = 3; // 3
+
+add(z, elementToAdd); // 0x01, 3
+
+console.log(z); // Array(3) [1, 2, 4]
+console.log(elementToAdd); // 3
+
+function add(array, element) {
+  element = element + 1; // 4
+  array.push(element); // [1, 2, 4]
+}
