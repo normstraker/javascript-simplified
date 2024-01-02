cb0b08838ca15035bdffb72e1f50489be572d727
Norm Straker <normstraker@yahoo.com>
2024-01-02 12:23:32 -0600

    33 - New and This

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// String Template Literals
-let a = "Hello";
-let b = "World";
+// New and This
 
-console.log(`${a} ${b}`);
+// create a function that lets us create a user object
 
-// Exercise: create two variables, one for your first name and one for your last name, then combine them together with back tics.
+// function createUser(name, age){
+//   return {
+//     name: name,
+//     age: age
+//   }
+// }
 
-let firstName = "Norm";
-let lastName = "Straker";
+// const user = createUser('Norm', 25);
+// console.log(user);
 
-console.log(`${firstName} ${lastName}`); // template literal or template string
+// ======================
+// New
 
+// works just like an object
+// when you are creating an object with the new keyword, it's called a constructor function
+const date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMilliseconds());
 
+// when you create a function with a constructor, you generally capitalize the first letter
+
+// constructor function, because it constucts a new object of the specified type
+
+function User(name, age) {
+  // this = {}
+  this.name = name;
+  this.age = age;
+  this.human = true;
+  // return this
+}
+const user = new User("Norm", 25);
+console.log(user);
